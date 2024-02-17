956c150146d4f1108d11faf3567f87f34a5603ce
Vijay <107832744+vijay2110746@users.noreply.github.com>
2024-02-17 23:52:50 +0530

    Update to_do_list_using_kivy.py
    nearly completed everything

--- to_do_list_using_kivy.py
+++ to_do_list_using_kivy.py
@@ -6,6 +6,21 @@ from kivymd.uix.button import MDIconButton
 from kivymd.uix.selectioncontrol import MDCheckbox
 from kivymd.uix.card import MDCard
 from kivy.metrics import dp
+# from kivymd.uix.button import MB
+import mysql.connector
+
+
+global_username=None
+global_password=None
+datatask=None
+conn=mysql.connector.connect(host='localhost',user='root',password='vijay',database='login')
+try:
+    if conn.is_connected():
+        print("yes")
+except:
+    print("Errored")
+
+cur=conn.cursor()
 
 KV = '''
 Manager:
@@ -16,7 +31,7 @@ Manager:
     name:"first"
     MDCard:
         size_hint: None,None
-        size: 350,400
+        size: 450,500
         pos_hint: {"center_x":0.5,"center_y":0.5}
         elevation: 10
         padding:25
@@ -56,15 +71,22 @@ Manager:
             font_size: 18
             pos_hint:{'center_x':0.5}
             on_release: app.login(user.text,pass_input.text)
+
+        MDFlatButton:
+            text:'new user? SignUP'
+            font_size:14
+            pos_hint:{'center_x':0.5}
+            on_release: app.switch()
+
 <Login>:
     name:'second'
     MDCard:
         size_hint: None,None
-        size: 350,500
+        size: 450,600
         pos_hint: {"center_x":0.5,"center_y":0.5}
         elevation: 10
         padding:25
-        spacing:25
+        spacing:15
         orientation: 'vertical'
     
         MDLabel:
@@ -103,13 +125,29 @@ Manager:
             height:100
             font_size: 18
             pos_hint:{'center_x':0.5}
+
+        MDTextField:
+            id:pass_input2
+            hint_text:'password'
+            icon_right:'eye-off'
+            password:True
+            width: 200
+            height:100
+            font_size: 18
+            pos_hint:{'center_x':0.5}
         
         
         MDRaisedButton:
             text:"Sign Up"
             font_size: 18
             pos_hint:{'center_x':0.5}
-            on_press: app.signup(user.text,mail_input.text,pass_input.text)
+            on_press: app.signup(user.text,mail_input.text,pass_input1.text,pass_input2.text)
+        
+        MDFlatButton:
+            text:'already registered? SignIn'
+            font_size:14
+            pos_hint:{'center_x':0.5}
+            on_release: app.switch1()
 <ToDoList>:
     name:'third'
     MDBoxLayout:
@@ -129,6 +167,10 @@ Manager:
                 text: 'To-Do List'
                 halign: 'center'
                 font_style: 'H6'
+            
+            MDRaisedButton:
+                text: 'Save'
+                on_release: app.savedata()
         
         MDBoxLayout:
             orientation: 'horizontal'
@@ -176,18 +218,88 @@ class LoginApp(MDApp):
 
     def add_task(self, task_name):
         if task_name.strip():
+            print(task_name)
             task_card = TaskCard(task_name=task_name)
             self.root.get_screen('third').ids.task_list.add_widget(task_card)
             self.root.get_screen('third').ids.task_input.text = ''
 
-    def signup(self, username, email, password):
-        print(f'Username: {username}, Email: {email}, Password: {password} registered successfully.')
+    def switch(self):
+        self.root.current='second'
+
+    def switch1(self):
+        self.root.current='first'
+        
+    def signup(self, username, email, password,confirm):
+        global global_username,global_password
+        if password==confirm:
+            print(f'Username: {username}, Email: {email}, Password: {password} registered successfully.')
+            q = "INSERT INTO users (user, email, password) VALUES (%s, %s, %s)"
+            v=(username,email,password)
+            cur.execute(q,v)
+            conn.commit()
+            global_username=username
+            global_password=password
+            print('executed')
+            self.root.current='third'
+
 
     def login(self, username, password):
-        if username == 'admin' and password == 'password':
-            self.root.current='third'
+        global global_username,global_password
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users;")
+        res = cur.fetchall()
+        print(res)
+        username1=[]
+        password1=[]
+        for i in res:
+            username1.append(i[1])
+            password1.append(i[3])
+        if username in username1:
+            ind=username1.index(username)
+            if password1[ind]==password:
+                print("verified")
+                global_username=username
+                global_password=password
+                self.root.current='third'
+                q='select tasks from users where user = %s and password = %s'
+                v=(username,password)
+                cur.execute(q,v)
+                res=cur.fetchone()
+                # print(res[0])
+                r=res[0].split('::::::')
+                for i in r:
+                    self.add_task(i)
+
+
+            else:
+                print("Login credentials dont match!!!")
         else:
-            print('Login failed')
+            print("Username not in database!!!")
+    
+    def savedata(self):
+        tasks=[]
+        task_list=self.root.get_screen('third').ids.task_list
+        for widget in task_list.children:
+            if isinstance(widget,TaskCard):
+                tasks.append(widget.task_name)
+
+        print(tasks)
+        
+        r=''
+        ts=len(tasks)
+        for task in reversed(range(ts)):
+            if tasks[task]==tasks[0]:
+                r+=tasks[task]
+            else:
+                r+=tasks[task]+'::::::'
+        
+        if global_username and global_password:
+            q="update users set tasks = %s where password=%s and user = %s"
+            v=(r,global_password,global_username)
+            cur.execute(q,v)
+            conn.commit()
+
+        print('tasks ',tasks)
 
 class TaskCard(MDCard):
     def __init__(self, task_name, **kwargs):
@@ -211,6 +323,9 @@ class TaskCard(MDCard):
     def delete_task(self, instance):
         app = MDApp.get_running_app()
         app.root.get_screen('third').ids.task_list.remove_widget(self)
+        # print(app.root.get_screen('third').ids.task_list)
+        
+    
 
 if __name__ == '__main__':
     LoginApp().run()
