6509c7c4ff2fbfc5247266e9c50dedc1345b2dd4
Vijay <107832744+vijay2110746@users.noreply.github.com>
2024-02-17 19:38:31 +0530

    Update to_do_list_using_kivy.py
    created login and sign up forms and made the hyperlinking and all

--- to_do_list_using_kivy.py
+++ to_do_list_using_kivy.py
@@ -1,15 +1,117 @@
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
-from kivy.uix.scrollview import ScrollView
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
-from kivymd.uix.button import MDIconButton
-from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.label import MDLabel
-from kivymd.uix.list import MDList, TwoLineListItem
-from kivymd.uix.textfield import MDTextField
+from kivymd.uix.button import MDIconButton
+from kivymd.uix.selectioncontrol import MDCheckbox
+from kivymd.uix.card import MDCard
+from kivy.metrics import dp
 
 KV = '''
-Screen:
+Manager:
+    SignUp:
+    Login:
+    ToDoList:
+<SignUp>:
+    name:"first"
+    MDCard:
+        size_hint: None,None
+        size: 350,400
+        pos_hint: {"center_x":0.5,"center_y":0.5}
+        elevation: 10
+        padding:25
+        spacing:25
+        orientation: 'vertical'
+    
+        MDLabel:
+            id: Sign_In
+            text: "Sign In"
+            font_size:40
+            halign:'center'
+            size_hint_y: None
+            height: self.texture_size[1]
+            padding:15
+        
+        MDTextField:
+            id:user
+            hint_text:'username'
+            icon_right:'account'
+            width: 200
+            height:100
+            font_size: 18
+            pos_hint:{'center_x':0.5}
+        MDTextField:
+            id:pass_input
+            hint_text:'password'
+            icon_right:'eye-off'
+            password:True
+            width: 200
+            height:100
+            font_size: 18
+            pos_hint:{'center_x':0.5}
+        
+        
+        MDRaisedButton:
+            text:"Log In"
+            font_size: 18
+            pos_hint:{'center_x':0.5}
+            on_release: app.login(user.text,pass_input.text)
+<Login>:
+    name:'second'
+    MDCard:
+        size_hint: None,None
+        size: 350,500
+        pos_hint: {"center_x":0.5,"center_y":0.5}
+        elevation: 10
+        padding:25
+        spacing:25
+        orientation: 'vertical'
+    
+        MDLabel:
+            id: Sign_Up
+            text: "Sign Up"
+            font_size:40
+            halign:'center'
+            size_hint_y: None
+            height: self.texture_size[1]
+            padding:15
+        
+        MDTextField:
+            id:user
+            hint_text:'username'
+            icon_right:'account'
+            width: 200
+            height:100
+            font_size: 18
+            pos_hint:{'center_x':0.5}
+
+        MDTextField:
+            id:mail_input
+            hint_text:'email'
+            icon_right:'email'
+            width: 200
+            height:100
+            font_size: 18
+            pos_hint:{'center_x':0.5}
+            
+        MDTextField:
+            id:pass_input1
+            hint_text:'password'
+            icon_right:'eye-off'
+            password:True
+            width: 200
+            height:100
+            font_size: 18
+            pos_hint:{'center_x':0.5}
+        
+        
+        MDRaisedButton:
+            text:"Sign Up"
+            font_size: 18
+            pos_hint:{'center_x':0.5}
+            on_press: app.signup(user.text,mail_input.text,pass_input.text)
+<ToDoList>:
+    name:'third'
     MDBoxLayout:
         orientation: 'vertical'
         
@@ -38,8 +140,12 @@ Screen:
             MDTextField:
                 id: task_input
                 hint_text: "Enter task name"
-                size_hint_x: None
                 width: root.width - dp(150)
+                hint_text_color: app.theme_cls.disabled_hint_text_color
+                line_color_normal: app.theme_cls.primary_color
+                mode: "rectangle"
+                color_mode: "primary"
+                on_text_validate: app.add_task(self.text)
                 
             MDIconButton:
                 icon: 'plus'
@@ -48,34 +154,63 @@ Screen:
         ScrollView:
             MDList:
                 id: task_list
+                spacing: dp(16)
 '''
 
+class SignUp(Screen):
+    pass
 
-class TaskListItem(TwoLineListItem):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.description_input = MDTextField(hint_text="Enter task description", multiline=True)
-        self.secondary_text = ''
+class Login(Screen):
+    pass
 
-    def add_description_input(self):
-        if not self.secondary_text:
-            self.add_widget(self.description_input)
-            self.secondary_text = self.description_input
+class ToDoList(Screen):
+    pass
 
+class Manager(ScreenManager):
+    pass
 
-class ToDoListApp(MDApp):
-    task_count = 0
-
-    def add_task(self, task_name):
-        if task_name.strip():
-            self.task_count += 1
-            task_item = TaskListItem(text=task_name)
-            task_item.add_description_input()  # Add description input when adding task
-            self.root.ids.task_list.add_widget(task_item)
-
+class LoginApp(MDApp):
+    task_count=0
     def build(self):
         return Builder.load_string(KV)
 
+    def add_task(self, task_name):
+        if task_name.strip():
+            task_card = TaskCard(task_name=task_name)
+            self.root.get_screen('third').ids.task_list.add_widget(task_card)
+            self.root.get_screen('third').ids.task_input.text = ''
+
+    def signup(self, username, email, password):
+        print(f'Username: {username}, Email: {email}, Password: {password} registered successfully.')
+
+    def login(self, username, password):
+        if username == 'admin' and password == 'password':
+            self.root.current='third'
+        else:
+            print('Login failed')
+
+class TaskCard(MDCard):
+    def __init__(self, task_name, **kwargs):
+        super().__init__(**kwargs)
+        self.task_name = task_name
+        self.orientation = 'horizontal'
+        self.size_hint_y = None
+        self.height = dp(72)
+        self.spacing = dp(8)
+        self.elevation=dp(6)
+        
+        checkbox = MDCheckbox()
+        task_label = MDLabel(text=task_name)
+        delete_button = MDIconButton(icon='delete')
+        delete_button.bind(on_release=self.delete_task)
+        
+        self.add_widget(checkbox)
+        self.add_widget(task_label)
+        self.add_widget(delete_button)
+    
+    def delete_task(self, instance):
+        app = MDApp.get_running_app()
+        app.root.get_screen('third').ids.task_list.remove_widget(self)
 
 if __name__ == '__main__':
-    ToDoListApp().run()
+    LoginApp().run()
